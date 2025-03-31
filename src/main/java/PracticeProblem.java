a9af755ba5694ab8f91a98dd74a2179e3e2c413c
Mayurun-Ravindramoorthi-ics4u1-03-25 <mayurun.ravindramoorthi@student.tdsb.on.ca>
2025-03-31 00:09:32 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,21 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+    public static void insertionSort(char[] characters) {
+        
+        for (int i = 1; i < characters.length; i++) {
+            char key = characters[i];  
+            int j = i - 1;
 
+         
+            while (j >= 0 && characters[j] > key) {
+                characters[j + 1] = characters[j];
+                j = j - 1;
+            }
+
+          
+            characters[j + 1] = key;
+        }
+    }
 
 }
